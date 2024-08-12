eaff5f74afe82e4f9105b55eb58e9b1fdf74b44f
felimarod <felipemartinrod@gmail.com>
2024-08-12 03:45:34 -0500

    feat: se agregan codigos de reformulación https://github.com/udistrital/planeacion_cliente/issues/1271

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,9 @@
 export const environment = {
   SECRET_KEY: 'MySecretKey',
+  // PLANES_CRUD: 'http://localhost:8080/',
+  // PLANES_CRUD: 'http://pruebasapi2.intranetoas.udistrital.edu.co:8523/',
   PLANES_CRUD: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/planes_crud/v1/',
-  PARAMETROS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/parametros/v1/',
+  // PARAMETROS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/parametros/v1/',
+  PARAMETROS_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8510/v1/',
+  DOCUMENTO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/documento_crud/v2/',
 }
